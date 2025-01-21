c3060994457639ba8d86574d888f3add7fc82ffd
Kye-Lee <kyelee.low@gmail.com>
2025-01-21 15:12:18 +0000

    set up express server

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -5,3 +5,7 @@ const app = express();
 app.get('/hello', function(req, res) { //(request, response)
     res.send('Hello!');
 });
+
+app.listen(8000, function() {
+    console.log('Server is listening on port 8000')
+});

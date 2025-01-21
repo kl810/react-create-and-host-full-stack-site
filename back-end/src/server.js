a08ba3d5a7ac1286523142d05ff9af2d2cd94ba5
Kye-Lee <kyelee.low@gmail.com>
2025-01-21 15:51:44 +0000

    route parameters

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,9 +2,19 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json()) //to use json body for 'req.body'
+
 app.get('/hello', function(req, res) { //(request, response)
-    res.send('Hello!');
+    res.send('Hello from a GET endpoint!');
 });
+
+app.get('/hello/:name', function(req, res) {
+    res.send('Hello, ' + req.params.name) //'name' derived from /hello/:name
+})
+
+app.post('/hello', function(req, res) {
+    res.send('Hello, ' + req.body.name + ' from a POST endpoint!')
+})
 
 app.listen(8000, function() {
     console.log('Server is listening on port 8000')

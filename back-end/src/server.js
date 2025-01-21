32f0d1cbdc6c0af30b264f2f728fb75a3d83ab3f
Kye-Lee <kyelee.low@gmail.com>
2025-01-21 16:10:03 +0000

    increase upvote on POST request

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,19 +1,32 @@
 import express from 'express';
 
+const articleInfo = [
+    { name: 'learn-node', upvotes: 0},
+    { name: 'learn-react', upvotes: 0},
+    { name: 'learn-mongodb', upvotes: 0},
+]
+
 const app = express();
 
 app.use(express.json()) //to use json body for 'req.body'
 
-app.get('/hello', function(req, res) { //(request, response)
-    res.send('Hello from a GET endpoint!');
-});
+// app.get('/hello', function(req, res) { //(request, response)
+//     res.send('Hello from a GET endpoint!');
+// });
 
-app.get('/hello/:name', function(req, res) {
-    res.send('Hello, ' + req.params.name) //'name' derived from /hello/:name
-})
+// app.get('/hello/:name', function(req, res) {
+//     res.send('Hello, ' + req.params.name) //'name' derived from /hello/:name
+// })
 
-app.post('/hello', function(req, res) {
-    res.send('Hello, ' + req.body.name + ' from a POST endpoint!')
+// app.post('/hello', function(req, res) {
+//     res.send('Hello, ' + req.body.name + ' from a POST endpoint!')
+// })
+
+app.post('/api/articles/:name/upvote', (req, res) => {
+    const article = articleInfo.find(a => a.name === req.params.name)
+    article.upvotes += 1;
+
+    res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!')
 })
 
 app.listen(8000, function() {

8a65d9b3b742f1401c9f887010d67a52cde56d80
Kye-Lee <kyelee.low@gmail.com>
2025-01-21 22:28:49 +0000

    created upvote button and useState hook used to update number of upvotes

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,4 +1,5 @@
 import { useParams, useLoaderData } from "react-router-dom";
+import { useState } from "react";
 import axios from 'axios';
 import articles from "../article-content";
 import CommentsList from "../commentsList";
@@ -7,13 +8,21 @@ export default function ArticlePage() {
 
     const { name } = useParams(); {/* same as const params = useParams()
                                         const name = params.name */}
-    const { upvotes, comments } = useLoaderData();
+    const { upvotes: initialUpvotes, comments } = useLoaderData();
+    const [ upvotes, setUpvotes ] = useState(initialUpvotes)
 
     const article = articles.find(a => a.name === name);
+
+    async function onUpvoteClicked() {
+        const response = await axios.post('/api/articles/' + name + '/upvote');
+        const updatedArticleData = response.data;
+        setUpvotes(updatedArticleData.upvotes)
+    }
 
     return (
         <>
             <h1>{article.title}</h1>
+            <button onClick={onUpvoteClicked}>Upvote</button>
             <p>This article has {upvotes} upvotes!</p>
             {article.content.map(p => <p key={p}>{p}</p>)}
             <CommentsList comments={comments} />

19fffcb419ac7e2e043ded16fe987403c6f7b1fe
Kye-Lee <kyelee.low@gmail.com>
2025-01-21 14:11:50 +0000

    creating and linking articles list

--- front-end/src/pages/ArticlesListPage.jsx
+++ front-end/src/pages/ArticlesListPage.jsx
@@ -1,16 +1,11 @@
-import { Link } from "react-router-dom";
+import ArticlesList from "../ArticlesList";
 import articles from "../article-content";
 
 export default function ArticlesListPage() {
     return (
         <>
             <h1>Articles</h1>
-            {articles.map(a => (
-                <Link key={a.name} to={'/articles/' + a.name}>
-                    <h3>{a.title}</h3>
-                    <p>{a.content[0].substring(0, 150)}</p>
-                </Link>
-            ))}
+            <ArticlesList articles={articles}/>
         </>
         
     );

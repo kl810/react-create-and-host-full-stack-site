94b00576d800d72f5951ad27ed054322db9bbebb
Kye-Lee <kyelee.low@gmail.com>
2025-01-21 21:31:46 +0000

    rewriting upvote endpoint

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ReturnDocument, ServerApiVersion } from 'mongodb';
 
 const articleInfo = [
     { name: 'learn-node', upvotes: 0, comments: []},
@@ -22,10 +22,9 @@ app.use(express.json()) //to use json body for 'req.body'
 // app.post('/hello', function(req, res) {
 //     res.send('Hello, ' + req.body.name + ' from a POST endpoint!')
 // })
+let db;
 
-app.get('/api/articles/:name', async (req, res) => {
-    const { name } = req.params;
-
+async function connectToDB() {
     const uri = 'mongodb://localhost:27017/';
 
     const client = new MongoClient(uri, {
@@ -38,20 +37,28 @@ app.get('/api/articles/:name', async (req, res) => {
 
     await client.connect();
 
-    const db = client.db('full-stack-react-db')
-    const article = await db.collection('articles').findOne({ name });
+    db = client.db('full-stack-react-db')
 
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    const { name } = req.params;
+    const article = await db.collection('articles').findOne({ name });
     res.json(article)
 });
 
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const article = articleInfo.find(a => a.name === req.params.name)
-    article.upvotes += 1;
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    const { name } = req.params;
+    const updatedArticle = await db.collection('articles').findOneAndUpdate({ name }, {
+        $inc: { upvotes: 1 } //MongoDB syntax - inc = increment
+    }, {
+        ReturnDocument: "after",
+    })
 
     // res.send('Success! The article ' + req.params.name + ' now has ' + article.upvotes + ' upvotes!')
 
-    res.json(article);
+    res.json(updatedArticle);
 })
 
 app.post('/api/articles/:name/comments', (req, res) => {
@@ -68,6 +75,13 @@ app.post('/api/articles/:name/comments', (req, res) => {
     res.json(article);
 })
 
-app.listen(8000, function() {
-    console.log('Server is listening on port 8000')
-});
+async function start() {        //call connectToDB before starting up server
+    await connectToDB();
+
+    app.listen(8000, function() {
+        console.log('Server is listening on port 8000')
+    });
+}
+
+start();
+

300b54cc6dbe14fc7859471528f8d641558eb14f
Kye-Lee <kyelee.low@gmail.com>
2025-01-24 23:06:32 +0000

    added app.yaml file

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import articles from "../article-content";
 import CommentsList from "../commentsList";
 import AddCommentForm from "../AddCommentForm";
+import useUser from '../useUser';
 
 export default function ArticlePage() {
 
@@ -11,21 +12,27 @@ export default function ArticlePage() {
                                         const name = params.name */}
     const { upvotes: initialUpvotes, comments: initialComments } = useLoaderData();
     const [ upvotes, setUpvotes ] = useState(initialUpvotes);
-    const [ comments, setComments ] = useState(initialComments)
+    const [ comments, setComments ] = useState(initialComments);
+
+    const { isLoading, user } = useUser();
 
     const article = articles.find(a => a.name === name);
 
     async function onAddComment({ nameText, commentText }) {
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
         const response = await axios.post('/api/articles/' + name + '/comments', {
             postedBy: nameText,
             text: commentText,
-        });
+        }, { headers });
         const updatedArticleData = response.data;
         setComments(updatedArticleData.comments)
     }
 
     async function onUpvoteClicked() {
-        const response = await axios.post('/api/articles/' + name + '/upvote');
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.post('/api/articles/' + name + '/upvote', null, { headers });
         const updatedArticleData = response.data;
         setUpvotes(updatedArticleData.upvotes)
     }
@@ -33,10 +40,12 @@ export default function ArticlePage() {
     return (
         <>
             <h1>{article.title}</h1>
-            <button onClick={onUpvoteClicked}>Upvote</button>
+            {user && <button onClick={onUpvoteClicked}>Upvote</button>}
             <p>This article has {upvotes} upvotes!</p>
             {article.content.map(p => <p key={p}>{p}</p>)}
-            <AddCommentForm onAddComment={onAddComment} />
+            {user 
+                ? <AddCommentForm onAddComment={onAddComment} />
+                : <p>Log in to add comment</p>}
             <CommentsList comments={comments} />
         </>
         
